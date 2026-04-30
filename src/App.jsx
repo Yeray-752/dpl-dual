3772984cc0004592c1dfa48e7d2ea1d451856f2c
Yeray <yeray.carrion@telespazio.com>
2026-04-30 12:16:13 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'; 
+function App() { 
+return ( 
+<main className="container"> 
+<h1>Actividad DUAL 4 - CI/CD</h1> 
+<p>Aplicación web desplegada mediante un pipeline básico.</p> 
+<p>Alumno: Nombre Apellidos</p> 
+</main> 
+); 
+} 
+export default App; 
